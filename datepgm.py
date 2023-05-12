0897d98fc7d3ea6e5b51c662fe19e31e846ea081
Greeshmavarghese <greeshmavarghese@gmail.com>
2023-05-12 19:22:48 +1000

    commit message

---
+++ datepgm.py
@@ -0,0 +1,11 @@
+"Added h1 tag"
+day= input ('date:-')
+mon = input('month:-')
+year= input ('year:-')
+monind=  int(mon) - 1
+months= ['jan', 'feb', 'mar' ,'apr' ,'may','jun','jul','aug','sep','oct','nov','dec']
+dayin= int(day)-1
+monaph= months[monind]
+prefix =['st','nd','rd'] + 17*['th'] + ['st','nd','rd'] +7 *['th']+['st']
+daypre = prefix[dayin]
+print('date entered is %s%s  %s %s' %(day,daypre, monaph, year))
